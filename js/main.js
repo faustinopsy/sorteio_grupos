4d75259ea8bdd013805f6fc8f231bc43b2ad78bd
faustinopsy <rodrigohipnose@gmail.com>
2026-09-07 22:01:47 -0300

    primeira forma do sorteio

--- js/main.js
+++ js/main.js
@@ -10,14 +10,34 @@ const GRUPOS = [
       "WizTech Secret"
     ];
 
+const MUSEUS = [
+      "MAC-USP", 
+      "Memorial da Resistência", 
+      "CCBB SP", 
+      "Casa das Rosas", 
+      "Pinacoteca", 
+      "Museu do Futebol", 
+      "Museu da Língua Portuguesa", 
+      "Museu Afro Brasil", 
+      "Museu da Imigração", 
+      "Museu de Zoologia USP", 
+      "IMS Paulista", 
+      "Itaú Cultural", 
+      "Japan House", 
+      "Museu das Favelas", 
+      "CAIXA Cultural"
+    ];
 
 function mostrarGrupos() {
     const lista = document.getElementById("resultado");
     lista.innerHTML = "";
     GRUPOS.forEach(grupo => {
+        museusSorteados = MUSEUS.sort(() => Math.random() - 0.5).slice(0, 2);
         const li = document.createElement("li");
-        li.textContent = grupo;
+        li.className = "bem-card bem-card--flat bem-p-sm bem-text-xs";
+        li.innerHTML = `<b>${grupo}</b> : ${museusSorteados.join(", ")}`;
         lista.appendChild(li);
     });
+       
 }
 mostrarGrupos()
