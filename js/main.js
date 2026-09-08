0458b081e2cda1ef73db07becce2eb73d5fa2aa2
faustinopsy <rodrigohipnose@gmail.com>
2026-09-07 22:07:46 -0300

    resolvendo duplicatas

--- js/main.js
+++ js/main.js
@@ -32,11 +32,22 @@ function sortear() {
     const lista = document.getElementById("resultado");
     lista.innerHTML = "";
     GRUPOS.forEach(grupo => {
-        museusSorteados = MUSEUS.sort(() => Math.random() - 0.5).slice(0, 1);
+        museusSorteados = selecionarMuseus();
         const li = document.createElement("li");
         li.className = "bem-card bem-card--flat bem-p-sm bem-text-xs";
         li.innerHTML = `<b>${grupo}</b> : ${museusSorteados.join(", ")}`;
         lista.appendChild(li);
     });
        
+}
+function selecionarMuseus(){
+    const museusSelecionados = [];
+    while (museusSelecionados.length < 1) {
+        const indiceAleatorio = Math.floor(Math.random() * MUSEUS.length);
+        const museuSorteado = MUSEUS[indiceAleatorio];
+              museusSelecionados.push(museuSorteado);
+              MUSEUS.splice(indiceAleatorio, 1);
+        
+    }
+    return museusSelecionados;
 }
